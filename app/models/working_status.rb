8d89f0ddb46e5465fc3dad8c683be9b0fc011368
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-19 19:44:43 +0900

    [add]working_statusモデル記入
    休日・希望休・出勤のみで作成

--- app/models/working_status.rb
+++ app/models/working_status.rb
@@ -1,2 +1,7 @@
 class WorkingStatus < ApplicationRecord
+  self.data = [
+    {id: 1, status: '休日', working_times: 0}, 
+    {id: 2, status: '希休', working_times: 0}, 
+    {id: 3, status: '出勤', working_times: 8}
+  ]
 end
