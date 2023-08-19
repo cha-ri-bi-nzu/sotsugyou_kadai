37d01826e9659cc0dc66db90047645daccf5bbcd
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-20 01:26:32 +0900

    [fix]シフト作成が途中で止まるエラー解消(期待通りの作成には失敗)
    privateアクションにて必要データの受け渡し設定(同時に共通処理の統一)、カラム名の間違い、同じメソッドを重複して使っていた個所の修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,22 +1,21 @@
 require "date"
 
 class AttendancesController < ApplicationController
-
+  before_action :set_group, only: %i[new create]
+  before_action :set_month, only: %i[create show]
+  before_action :set_group_sesired_holidays, only: [:create]
+  
   def index
   end
 
   def new
-    @group = Group.find(params[:group_id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
-    @month = Date.parse("#{params["month(1i)"]}-#{params["month(2i)"]}-#{params["month(3i)"]}")
     @days = []
     @month.all_month.each do |day|
       @days << day
     end
-    group_sesired_horydays = SesiredHoliday.where(group_id: @group.id).where("my_holiday >= ?", Date.parse((@month.beginning_of_month).to_s)).where("my_holiday <= ?", Date.parse((@month.end_of_month).to_s)).reorder(my_holiday: :asc)
     # binding.pry
     @days.each do |day|
       @group.users.each do |user|
@@ -26,7 +25,7 @@ class AttendancesController < ApplicationController
         @attendance.group_id = @group.id
         if day.wday == 0 || day.wday == 6
           @attendance.working_status_id = 1
-        elsif user_holiday_present(group_sesired_horydays, day, user)
+        elsif user_holiday_present(day, user)
           @attendance.working_status_id = 2
         else
           @attendance.working_status_id = 3
@@ -34,17 +33,12 @@ class AttendancesController < ApplicationController
         @attendance.save
       end
     end
-    redirect_to attendance_path(Attendance.where(group_id: @group.id).find_by(my_holiday: Date.parse("#{@month.beginning_of_month}")).id)
+    redirect_to attendance_path(Attendance.where(group_id: @group.id).find_by(working_day: Date.parse("#{@month.beginning_of_month}")).id)
   end
 
   def show
-    if params["month(1i)"].present?
-      month = Date.parse("#{params["month(1i)"]}-#{params["month(2i)"]}-#{params["month(3i)"]}")
-    else
-      month = Date.parse("#{Attendance.find(params[:id])}")
-    end
     @group_users = User.all
-    @attendances = Attendance.where(user_id: @group_users.ids).where("working_day >= ?", DateTime.parse("#{@month.beginning_of_month}").beginning_of_month).where("working_day <= ?", DateTime.parse("#{@month.end_of_month}").end_of_month).order(user_id: :asc).order(working_day: :asc)
+    @attendances = Attendance.where(user_id: @group_users.ids).where("working_day >= ?", Date.parse("#{@month.beginning_of_month}")).where("working_day <= ?", Date.parse("#{@month.end_of_month}")).order(user_id: :asc).order(working_day: :asc)
     @days = []
     @attendances.first.working_day.all_month.each do |d|
       @days << d
@@ -61,5 +55,33 @@ class AttendancesController < ApplicationController
   end
 
   private
-  
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def set_group_sesired_holidays
+    set_group
+    @group_sesired_holidays = SesiredHoliday.where(group_id: @group.id).where("my_holiday >= ?", Date.parse((@month.beginning_of_month).to_s)).where("my_holiday <= ?", Date.parse((@month.end_of_month).to_s)).reorder(my_holiday: :asc)
+  end
+
+  def set_month
+    if params["month(1i)"].present?
+      @month = Date.parse("#{params["month(1i)"]}-#{params["month(2i)"]}-#{params["month(3i)"]}")
+    else
+      @month = Date.parse("#{Attendance.find(params[:id]).working_day}")
+    end
+  end
+
+  def user_holiday_present(a_day, user)
+    set_group_sesired_holidays
+    user_holidays = @group_sesired_holidays.where(user_id: user.id)
+    user_holidays.each_with_index do |holiday, i|
+      binding.pry
+      if holiday.my_holiday == a_day.day
+        return true
+      elsif i == user_holidays.length - 1
+        false
+      end
+    end
+  end 
 end
