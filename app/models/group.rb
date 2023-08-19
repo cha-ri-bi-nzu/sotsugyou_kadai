710efe5d742b9a1ca173f466b54424b88b95369a
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-19 11:44:07 +0900

    [add]attendancesテーブルのアソシエーション設定
    users・groupsテーブルとの紐付け完了

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   has_many :users, through: :groupings, source: :user
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   has_many :sesired_holidays, dependent: :destroy
+  has_many :attendances, dependent: :destroy
 end
