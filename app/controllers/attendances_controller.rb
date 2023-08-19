27a31c02fead0612d6d983b46e56cdfcdf3650ce
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-19 17:29:01 +0900

    [add]動作確認用アクション作成
    createアクション・viewファイルにパラメータ確認用のコードを記載

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -7,6 +7,9 @@ class AttendancesController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    month = "#{params["month(1i)"]}-#{params["month(2i)"]}-#{params["month(3i)"]}"
+    @month = Date.parse(month)
   end
 
   def show
