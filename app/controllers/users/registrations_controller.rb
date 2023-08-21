3da5e4cf94c3aeff053ee15623edca66eeac0ef7
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-21 16:12:01 +0900

    [add]ゲストログインユーザーがアカウント編集・削除できないよう制限
    privateメソッドにてメールアドレスで判定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :, only: %i[update destroy]
 
   # GET /resource/sign_up
   # def new
@@ -38,7 +39,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -59,4 +60,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  def ensure_normal_user
+    if ["guest@example.com", "admin_guest@example.com"].include?(resource.email)
+      redirect_to root_path, alert: "ゲストユーザーのアカウントは編集・削除できません。"
+    end
+  end
 end
