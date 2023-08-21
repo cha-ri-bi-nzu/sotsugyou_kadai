3686fd720021aa8cf7a355ee8882358b7578cfb0
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-21 15:04:11 +0900

    [add]deviseの関連コントローラ用ルーティング設定
    ゲストログインに必要なコントローラのみ定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'tops#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   resources :users, only: [:show]
 
   devise_scope :user do
