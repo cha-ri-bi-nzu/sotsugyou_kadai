a67d5cfb1a32f9cfa042f427a5c39fd2c2be06ad
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-22 17:54:34 +0900

    [add]各モデルにバリデーション設定
    上記の通り

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,9 @@
 class Attendance < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :working_day, presence: true
+  validates :working_status_id, presence: true
+
   belongs_to :user
   belongs_to :group
   belongs_to :working_status
