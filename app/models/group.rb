4edf0b93f09193304a5af4c1fcbc0a7ad6832d9f
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-22 16:48:19 +0900

    [add]groupのmoelspec成功
    groupのnameのバリデーションを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
+  validates :name, presence: true
+
   has_many :groupings, dependent: :destroy
   has_many :users, through: :groupings, source: :user
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
