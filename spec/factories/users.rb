016071a1cc2db1b9754cbafdafb0460cc5a64176
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-22 19:50:19 +0900

    [add]テスト用userデータ追加
    メールアドレスの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
     name {"admin_user"}
-    email {"admin@mail.com"}
+    sequence(:email) { |n| "test#{n}@example.com" }
     password {"adminadmin"}
     admin {true}
+  end
+  factory :second_user, class: User do
+    name {'user_name2'}
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password {'bbbbbb'}
+    admin {false}
   end
 end
