81e4f1649ace5e20aab95053f7e57fb6a9745a13
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-22 14:58:02 +0900

    [add]rspecの前準備6
    ファイル作成の修正

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,13 @@ module SotsugyouApp
 
     config.generators do |g|
       g.test_framework :rspec,
-                       model_specs: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: false,
-                       request_specs: false
+        model_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
