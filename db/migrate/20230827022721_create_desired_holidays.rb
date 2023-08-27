181d27321328240ccbf8ea197580c282e0917f1b
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-27 11:31:02 +0900

    [add]desired_holidayモデル作成
    初期状態保存

---
+++ db/migrate/20230827022721_create_desired_holidays.rb
@@ -0,0 +1,11 @@
+class CreateDesiredHolidays < ActiveRecord::Migration[6.1]
+  def change
+    create_table :desired_holidays do |t|
+      t.date :my_holiday
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
