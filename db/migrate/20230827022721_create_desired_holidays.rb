28fedcf0852a4e2ae3852252edcb66729bb0baa3
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-27 11:40:58 +0900

    [add]desired_holidayテーブル作成
    sesired_holidayはまだあり

--- db/migrate/20230827022721_create_desired_holidays.rb
+++ db/migrate/20230827022721_create_desired_holidays.rb
@@ -1,7 +1,7 @@
 class CreateDesiredHolidays < ActiveRecord::Migration[6.1]
   def change
     create_table :desired_holidays do |t|
-      t.date :my_holiday
+      t.date :my_holiday, index: true, null: false
       t.references :user, null: false, foreign_key: true
       t.references :group, null: false, foreign_key: true
 
