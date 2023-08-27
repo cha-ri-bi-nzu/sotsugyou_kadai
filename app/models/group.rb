1cc2a2802ff8bfa6e6e43987c4409b3df67b2d45
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-27 11:45:32 +0900

    [add]desired_holidayのアソシエーション設定
    sesired_holidayをそのまま流用

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,6 @@ class Group < ApplicationRecord
   has_many :groupings, dependent: :destroy
   has_many :users, through: :groupings, source: :user
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
-  has_many :sesired_holidays, dependent: :destroy
+  has_many :desired_holidays, dependent: :destroy
   has_many :attendances, dependent: :destroy
 end
