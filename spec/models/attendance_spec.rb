7b14d0f8cb4cc45821b04ea325bc8ab722d7ee29
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-11-27 22:43:34 +0900

    [clean]コメントアウト解除
    エラー解消時にコメントアウトしていたコードを戻し忘れていた

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Attendance, type: :model do
   describe 'バリデーションのテスト' do
     let!(:user){FactoryBot.create(:user)}
     let!(:group){FactoryBot.create(:group, owner_id: user.id)}
-    # context '全ての情報が入力された場合' do
-    #   it '希望休が作成される' do
-    #     attendance = Attendance.new(working_day: '2023-10-01', working_status_id: 1, user: user, group: group)
-    #     expect(attendance).to be_valid
-    #   end
-    # end
+    context '全ての情報が入力された場合' do
+      it '希望休が作成される' do
+        attendance = Attendance.new(working_day: '2023-10-01', working_status_id: 1, user: user, group: group)
+        expect(attendance).to be_valid
+      end
+    end
     context '勤怠の日付が空の場合' do
       it '勤怠情報が作成されない' do
         attendance = Attendance.new(working_day: '', working_status_id: 1, user: user, group: group)
