05feb157c7a2ab11f3af572b1a32951ed5129564
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-12-06 17:20:14 +0900

    [update]ファクトリーボット修正
    各idの定義変更

--- spec/factories/groupings.rb
+++ spec/factories/groupings.rb
@@ -1,32 +1,32 @@
 FactoryBot.define do
   factory :grouping do
     leave_group {false}
-    group_id {1}
-    user_id {1}
+    group_id {group.id}
+    user_id {user.id}
   end
   factory :second_grouping, class: Grouping do
     leave_group {false}
-    group_id {1}
-    user_id {2}
+    group_id {group.id}
+    user_id {second_user.id}
   end
   factory :third_grouping, class: Grouping do
     leave_group {false}
-    group_id {1}
+    group_id {group.id}
     user_id {3}
   end
   factory :fourth_grouping, class: Grouping do
     leave_group {false}
-    group_id {2}
-    user_id {1}
+    group_id {second_group.id}
+    user_id {user.id}
   end
   factory :fifth_grouping, class: Grouping do
     leave_group {false}
-    group_id {2}
-    user_id {2}
+    group_id {second_group.id}
+    user_id {second_user.id}
   end
   factory :sixth_grouping, class: Grouping do
     leave_group {true}
     group_id {3}
-    user_id {2}
+    user_id {second_user.id}
   end
 end
