42d2e4b56554cb9e59b0d0234ca54e3cec26b593
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-12-06 22:41:36 +0900

    [clean]初期設定のリファクタリング
    ファクトリーボット適応、ログイン作業をbeforeで1つに集約

--- spec/system/group_spec.rb
+++ spec/system/group_spec.rb
@@ -3,14 +3,18 @@ require 'rails_helper'
 RSpec.describe 'グループ機能', type: :system do
   describe '新規作成機能' do
     let!(:user) {FactoryBot.create(:user)}
-    let!(:group) {FactoryBot.create(:group, owner_id: user.id)}
-    let!(:grouping) {FactoryBot.create(:grouping, user_id: user.id, group_id: group.id)}
+    let!(:third_user) {FactoryBot.create(:third_user)}
+    let!(:group) {FactoryBot.create(:group)}
+    let!(:grouping) {FactoryBot.create(:grouping)}
+    let!(:third_grouping) {FactoryBot.create(:third_grouping)}
+    before do
+      visit new_user_session_path
+      fill_in "user_email", with: user.email
+      fill_in "user[password]", with: 'adminadmin'
+      click_button "ログイン"
+    end
     context 'グループを新規作成した場合' do
       it '新規グループが作成される。' do
-        visit new_user_session_path
-        fill_in "user_email", with: user.email
-        fill_in "user[password]", with: 'adminadmin'
-        click_button "ログイン"
         click_on "グループを作る"
         fill_in "group_name", with: 'test1_group'
         click_button "登録"
@@ -19,10 +23,6 @@ RSpec.describe 'グループ機能', type: :system do
     end
     context 'グループを編集した場合' do
       it 'グループ名が編集される。' do
-        visit new_user_session_path
-        fill_in "user_email", with: user.email
-        fill_in "user[password]", with: 'adminadmin'
-        click_button "ログイン"
         click_on "test_group"
         click_on "グループ編集"
         fill_in "group_name", with: 'test2_group'
@@ -32,10 +32,6 @@ RSpec.describe 'グループ機能', type: :system do
     end
     context 'グループ削除をした場合' do
       it 'グループが論理削除される。' do
-        visit new_user_session_path
-        fill_in "user_email", with: user.email
-        fill_in "user[password]", with: 'adminadmin'
-        click_button "ログイン"
         click_on "test_group"
         click_on "グループ削除"
         expect(page).to have_content 'test_groupを削除しました'
@@ -43,10 +39,6 @@ RSpec.describe 'グループ機能', type: :system do
     end
     context 'グループ完全削除をした場合' do
       it 'グループが完全に削除される。' do
-        visit new_user_session_path
-        fill_in "user_email", with: user.email
-        fill_in "user[password]", with: 'adminadmin'
-        click_button "ログイン"
         click_on "test_group"
         click_on "グループ完全削除"
         expect(page).to have_content 'test_groupを完全に削除しました'
